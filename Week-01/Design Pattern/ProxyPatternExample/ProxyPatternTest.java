3a1c13fb5a1b4ff58f0f509cbcb9e9572624cf6a
Shreyansh <shreyanshloop07@gmail.com>
2026-06-20 18:50:18 +0530

    Add Proxy Pattern implementation

---
+++ Week-01/Design Pattern/ProxyPatternExample/ProxyPatternTest.java
@@ -0,0 +1,10 @@
+public class ProxyPatternTest {
+    public static void main(String[] args) {
+        Image image1 = new ProxyImage("photo1.jpg");
+        Image image2 = new ProxyImage("photo2.jpg");
+
+        image1.display();
+        image1.display();
+        image2.display();
+    }
+}
