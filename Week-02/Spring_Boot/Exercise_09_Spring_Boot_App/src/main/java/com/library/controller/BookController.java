b1545a3516e850e367d8ea957e894aa15aea7c52
Shreyansh <shreyanshloop07@gmail.com>
2026-06-29 14:12:45 +0530

    Week 02: Exercise 09 - Spring Boot Library Management Application

---
+++ Week-02/Spring_Boot/Exercise_09_Spring_Boot_App/src/main/java/com/library/controller/BookController.java
@@ -0,0 +1,34 @@
+package com.library.controller;
+
+import com.library.entity.Book;
+import com.library.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<Book> addBook(@RequestBody Book book) {
+        return ResponseEntity.ok(bookService.saveBook(book));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks() {
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBook(@PathVariable Long id) {
+        Book book = bookService.getBookById(id);
+        if (book == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(book);
+    }
+}
---
+++ Week-02/Spring_Boot/Exercise_09_Spring_Boot_App/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,25 @@
+package com.library.service;
+
+import com.library.entity.Book;
+import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+}
